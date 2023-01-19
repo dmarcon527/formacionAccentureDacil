968ecc48f0d7aaf6342f26cfc826e674b40659c9
dmarcon527 <dmarcon527@g.educaand.es>
2023-01-19 12:35:12 +0100

    ejercicios jueves

---
+++ Definiciones/src/Ejercicio12/CalcularFactorial.java
@@ -0,0 +1,28 @@
+package Ejercicio12;
+
+/**
+ * 
+ * @author Dácil Marco Contreras
+ *
+ */
+
+public class CalcularFactorial {
+	/**
+	 * Calcular el factorial del número 8
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		int fact = 8;
+		int num = 1;
+		int i = 1;
+		while (i <= fact) {
+			num = num * i;
+			i++;
+		}
+		System.out.println("El factorial del número" + fact + " es " + num);
+
+	}
+
+}
---
+++ Definiciones/src/Ejercicio12/ListarNumeros.java
@@ -0,0 +1,25 @@
+package Ejercicio12;
+
+/**
+ * 
+ * @author Dácil Marco Contreras
+ *
+ */
+public class ListarNumeros {
+	/**
+	 * Este programa lo que hace es mostrar los números del 100 al 0 mostrandolos de
+	 * 23 en 23
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		for (int i = 100; i > 0; i -= 23) {
+			System.out.println(i);
+
+		}
+
+	}
+
+}
