9f4a857b42bbf398bc1d6471804d541c8c6018da
dmarcon527 <dmarcon527@g.educaand.es>
2023-01-31 18:42:51 +0100

    cronometro mejorado

--- SemanaTres/src/ejercicioReloj/Reloj.java
+++ SemanaTres/src/ejercicioReloj/Reloj.java
@@ -1,38 +1,36 @@
 package ejercicioReloj;
 
+import java.text.DecimalFormat;
+
+/**
+ * 
+ * @author Dácil Marco Contreras
+ *
+ */
 public class Reloj {
+	static final int MAX_HORS = 24;
+	static final int MAX_MIN_SEG = 59;
+
+	/**
+	 * mostramos por pantalla un cronómetro
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 */
 
 	public static void main(String[] args) throws InterruptedException {
-		int hora =0;   
-		int minutos=0; 
-		int segundos=0; 
-		int limite=59; 
-		
-		for (int i=0; i<=limite; i++){
-			if(segundos<limite) {
-				segundos++; 
-					
-			}else if(minutos<limite) {
-					minutos++; 
-				}else {
-					hora++; 
-					minutos=0;
-					i=0; 
-				}
-				//if(i==limite) {
-				//	i=0; 
-				//	segundos=0; 
-				//}
-				if(i<9) {
-					System.out.println("0"+hora+":0"+minutos+":0"+segundos);
-				}else {
-					System.out.println(hora+":"+minutos+":"+segundos);
-				}
-			
-				Thread.sleep(10);
-		//Thread.sleep(1000); 
+		DecimalFormat formatoHora = new DecimalFormat("00");
 
+		for (int hora = 0; hora < MAX_HORS; hora++) {
+			for (int minutos = 0; minutos < MAX_MIN_SEG; minutos++) {
+				for (int segundos = 0; segundos < MAX_MIN_SEG; segundos++) {
+					System.out.println(formatoHora.format(hora) + ":" + formatoHora.format(minutos) + ":"
+							+ formatoHora.format(segundos));
+					Thread.sleep(1000);
+				}
+
+			}
 		}
+
 	}
 }
-
