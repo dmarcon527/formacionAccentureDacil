4c22034aa02d43a74e1efbac29d1f5b92209ae3f
dmarcon527 <dmarcon527@g.educaand.es>
2023-01-31 18:03:23 +0100

    baraja

---
+++ SemanaTres/src/barajaDeCartas/Jugador.java
@@ -0,0 +1,47 @@
+package barajaDeCartas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Jugador {
+
+	private String nombre;
+	private int edad;
+	private Set<Carta> manocartas;
+
+	public Jugador() {
+		manocartas = new HashSet<>();
+	}
+
+	public Set<Carta> getBaraja() {
+		return manocartas;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public void recibirCarta(Carta carta) {
+		manocartas.add(carta);
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", edad=" + edad + ", manocartas=" + manocartas + "]";
+	}
+	
+	
+
+}
